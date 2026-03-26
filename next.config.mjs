dc249361f6614079ac9cd62ce5e7c76a51cb2fc1
dreeydesigns <dreeydesigns@gmail.com>
2026-03-26 04:44:40 +0300

    Fix Railway standalone startup

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: '**' }
-    ]
-  }
 };
 
 export default nextConfig;
